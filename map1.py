886cf7be7553b41b7103f5639c91cc01bc65d8b1
subhahu123 <34541684+subhahu123@users.noreply.github.com>
2019-06-06 03:38:23 +0530

    On branch master

--- map1.py
+++ map1.py
@@ -4,13 +4,22 @@ import pandas
 data = pandas.read_csv("3.1 Volcanoes.txt.txt")
 lat = list(data["LAT"])
 lon = list(data["LON"])
+elev = list(data["ELEV"])
+
+def color_producer(el):
+    if el < 1000:
+        return 'green'
+    elif 1000 <= el < 3000:
+        return 'orange'
+    else:
+        return 'red'
 
 map = folium.Map(location=[30.58, -99.09])
 
 fg = folium.FeatureGroup(name="My Map")
 
-for lt, ln in zip(lat, lon):
-    fg.add_child(folium.Marker(location=[lt, ln], popup="hi i m a marker", icon=folium.Icon(color="green")))
+for lt, ln, el in zip(lat, lon, elev):
+    fg.add_child(folium.Marker(location=[lt, ln], popup=el, icon=folium.Icon(color=color_producer(el))))
 
 map.add_child(fg)
 
