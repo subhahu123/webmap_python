471a4d784e1046287e32540899d6a53ca7e2d461
subhahu123 <34541684+subhahu123@users.noreply.github.com>
2019-06-07 17:23:25 +0530

    adding layer control panel

--- map1.py
+++ map1.py
@@ -16,16 +16,21 @@ def color_producer(el):
 
 map = folium.Map(location=[30.58, -99.09])
 
-fg = folium.FeatureGroup(name="My Map")
+fg = folium.FeatureGroup(name="volcanoes")
 
 for lt, ln, el in zip(lat, lon, elev):
     fg.add_child(folium.Marker(location=[lt, ln], popup=el, icon=folium.Icon(color=color_producer(el))))
 
-fg.add_child(folium.GeoJson(data=open('12.1 world.json.json', 'r', encoding='utf-8-sig').read(), 
+fgp = folium.FeatureGroup(name="population")
+
+fgp.add_child(folium.GeoJson(data=open('12.1 world.json.json', 'r', encoding='utf-8-sig').read(), 
 style_function=lambda x: {'fillColor':'green' if x['properties']['POP2005'] < 10000000
 else 'orange' if 10000000 <= x['properties']['POP2005'] < 20000000 else 'red'}))
 
+
 map.add_child(fg)
+map.add_child(fgp)
+map.add_child(folium.LayerControl())
 
 map.save("new.html")
 
