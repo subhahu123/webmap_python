bb74426fc265f687c4b6737b415f0e005f4802e7
subhahu123 <34541684+subhahu123@users.noreply.github.com>
2019-06-07 17:02:56 +0530

    adding geojson

--- map1.py
+++ map1.py
@@ -21,8 +21,12 @@ fg = folium.FeatureGroup(name="My Map")
 for lt, ln, el in zip(lat, lon, elev):
     fg.add_child(folium.Marker(location=[lt, ln], popup=el, icon=folium.Icon(color=color_producer(el))))
 
+fg.add_child(folium.GeoJson(data=open('12.1 world.json.json', 'r', encoding='utf-8-sig').read(), 
+style_function=lambda x: {'fillColor':'green' if x['properties']['POP2005'] < 10000000
+else 'orange' if 10000000 <= x['properties']['POP2005'] < 20000000 else 'red'}))
+
 map.add_child(fg)
 
-map.save("index.html")
+map.save("new.html")
 
 
